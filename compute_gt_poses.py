e1498d95e1d264442c89606e5fb4d88850692fd1
scirocco <zyf199966@126.com>
2019-07-22 20:55:10 +0800

    fix some python3 incompatibleness

--- compute_gt_poses.py
+++ compute_gt_poses.py
@@ -2,7 +2,7 @@
 compute_gt_poses.py
 ---------------
 
-Main Function for registering (aligning) colored point clouds with ICP/aruco marker 
+Main Function for registering (aligning) colored point clouds with ICP/aruco marker
 matching as well as pose graph optimizating, output transforms.npy in each directory
 
 """
@@ -23,6 +23,7 @@ import sys
 from config.registrationParameters import *
 import json
 import png
+from PIL import Image
 
 # Set up parameters for registration
 # voxel sizes use to down sample raw pointcloud for fast ICP
@@ -34,25 +35,27 @@ max_correspondence_distance_fine = voxel_size * 1.5
 # Voxel size for the complete mesh
 voxel_Radius = VOXEL_R
 
-# Point considered an outlier if more than inlier_Radius away from other points  
+# Point considered an outlier if more than inlier_Radius away from other points
 inlier_Radius = voxel_Radius * 2.5
 
 # search for up to N frames for registration, odometry only N=1, all frames N = np.inf
 N_Neighbours = K_NEIGHBORS
 
 
-def marker_registration(source,target):
+def marker_registration(source, target):
      cad_src, depth_src = source
      cad_des, depth_des = target
- 
+
      gray_src = cv2.cvtColor(cad_src, cv2.COLOR_RGB2GRAY)
      gray_des = cv2.cvtColor(cad_des, cv2.COLOR_RGB2GRAY)
      aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
      parameters = aruco.DetectorParameters_create()
-    
-     #lists of ids and the corners beloning to each id
-     corners_src, _ids_src, rejectedImgPoints = aruco.detectMarkers(gray_src, aruco_dict, parameters=parameters)
-     corners_des, _ids_des, rejectedImgPoints = aruco.detectMarkers(gray_des, aruco_dict, parameters=parameters)
+
+     # lists of ids and the corners beloning to each id
+     corners_src, _ids_src, rejectedImgPoints = aruco.detectMarkers(
+         gray_src, aruco_dict, parameters=parameters)
+     corners_des, _ids_des, rejectedImgPoints = aruco.detectMarkers(
+         gray_des, aruco_dict, parameters=parameters)
      try:
          ids_src = []
          ids_des = []
@@ -64,39 +67,37 @@ def marker_registration(source,target):
          return None
 
      common = [x for x in ids_src if x in ids_des]
-  
+
      if len(common) < 2:
           # too few marker matches, use icp instead
           return None
 
-     
      src_good = []
      dst_good = []
-     for i,id in enumerate(ids_des):
+     for i, id in enumerate(ids_des):
           if id in ids_src:
                j = ids_src.index(id)
-               for count,corner in enumerate(corners_src[j][0]):
+               for count, corner in enumerate(corners_src[j][0]):
                     feature_3D_src = depth_src[int(corner[1])][int(corner[0])]
-                    feature_3D_des = depth_des[int(corners_des[i][0][count][1])][int(corners_des[i][0][count][0])]
-                    if feature_3D_src[2]!=0 and feature_3D_des[2]!=0:
+                    feature_3D_des = depth_des[int(corners_des[i][0][count][1])][int(
+                        corners_des[i][0][count][0])]
+                    if feature_3D_src[2] != 0 and feature_3D_des[2] != 0:
                          src_good.append(feature_3D_src)
                          dst_good.append(feature_3D_des)
-    
+
      # get rigid transforms between 2 set of feature points through ransac
      try:
-          transform = match_ransac(np.asarray(src_good),np.asarray(dst_good))
+          transform = match_ransac(np.asarray(src_good), np.asarray(dst_good))
           return transform
      except:
           return None
-
-
 
 
 def post_process(originals, voxel_Radius, inlier_Radius):
      """
     Merge segments so that new points will not be add to the merged
     model if within voxel_Radius to the existing points, and keep a vote
-    for if the point is issolated outside the radius of inlier_Radius at 
+    for if the point is issolated outside the radius of inlier_Radius at
     the timeof the merge
 
     Parameters
@@ -107,7 +108,7 @@ def post_process(originals, voxel_Radius, inlier_Radius):
       Reject duplicate point if the new point lies within the voxel radius
       of the existing point
     inlier_Radius : float
-      Point considered an outlier if more than inlier_Radius away from any 
+      Point considered an outlier if more than inlier_Radius away from any
       other points
 
     Returns
@@ -117,7 +118,7 @@ def post_process(originals, voxel_Radius, inlier_Radius):
     colors : (n,3) float
       The (r,g,b) color information corresponding to the points
     vote : (n, ) int
-      The number of vote (seen duplicate points within the voxel_radius) each 
+      The number of vote (seen duplicate points within the voxel_radius) each
       processed point has reveived
     """
 
@@ -125,15 +126,17 @@ def post_process(originals, voxel_Radius, inlier_Radius):
 
           if point_id == 0:
                vote = np.zeros(len(originals[point_id].points))
-               points = np.array(originals[point_id].points,dtype = np.float64)
-               colors = np.array(originals[point_id].colors,dtype = np.float64)
+               points = np.array(originals[point_id].points, dtype=np.float64)
+               colors = np.array(originals[point_id].colors, dtype=np.float64)
 
           else:
-       
-               points_temp = np.array(originals[point_id].points,dtype = np.float64)
-               colors_temp = np.array(originals[point_id].colors,dtype = np.float64)
-               
-               dist , index = nearest_neighbour(points_temp, points)
+
+               points_temp = np.array(
+                   originals[point_id].points, dtype=np.float64)
+               colors_temp = np.array(
+                   originals[point_id].colors, dtype=np.float64)
+
+               dist, index = nearest_neighbour(points_temp, points)
                new_points = np.where(dist > voxel_Radius)
                points_temp = points_temp[new_points]
                colors_temp = colors_temp[new_points]
@@ -143,9 +146,10 @@ def post_process(originals, voxel_Radius, inlier_Radius):
                points = np.concatenate([points, points_temp])
                colors = np.concatenate([colors, colors_temp])
 
-     return (points,colors,vote) 
-  
-def full_registration(path,max_correspondence_distance_coarse,
+     return (points, colors, vote)
+
+
+def full_registration(path, max_correspondence_distance_coarse,
                       max_correspondence_distance_fine):
 
      global N_Neighbours, LABEL_INTERVAL, n_pcds
@@ -153,16 +157,16 @@ def full_registration(path,max_correspondence_distance_coarse,
      odometry = np.identity(4)
      pose_graph.nodes.append(PoseGraphNode(odometry))
 
-     pcds = [[] for i in range(n_pcds)]
-     for source_id in trange(n_pcds):
+     pcds = [[] for i in range(int(n_pcds))]
+     for source_id in trange(int(n_pcds)):
           if source_id > 0:
                pcds[source_id-1] = []
           # for target_id in range(source_id + 1, min(source_id + N_Neighbours,n_pcds)):
-          for target_id in range(source_id + 1, n_pcds, max(1,int(n_pcds/N_Neighbours))):
-               
+          for target_id in range(source_id + 1, int(n_pcds), max(1, int(n_pcds/N_Neighbours))):
+
                # derive pairwise registration through feature matching
-               color_src, depth_src  = load_images(path, source_id)
-               color_dst, depth_dst  = load_images(path, target_id)
+               color_src, depth_src = load_images(path, source_id)
+               color_dst, depth_dst = load_images(path, target_id)
                res = marker_registration((color_src, depth_src),
                                       (color_dst, depth_dst))
 
@@ -171,15 +175,17 @@ def full_registration(path,max_correspondence_distance_coarse,
                     continue
 
                if not pcds[source_id]:
-                    pcds[source_id] = load_pcd(path, source_id, downsample = True)
+                    pcds[source_id] = load_pcd(
+                        path, source_id, downsample=True)
                if not pcds[target_id]:
-                    pcds[target_id] = load_pcd(path, target_id, downsample = True)
-               
+                    pcds[target_id] = load_pcd(
+                        path, target_id, downsample=True)
+
                if res is None:
                     # if marker_registration fails, perform pointcloud matching
                     transformation_icp, information_icp = icp(
                          pcds[source_id], pcds[target_id], voxel_size, max_correspondence_distance_coarse,
-                         max_correspondence_distance_fine, method = ICP_METHOD)
+                         max_correspondence_distance_fine, method=ICP_METHOD)
 
                else:
                     transformation_icp = res
@@ -190,93 +196,93 @@ def full_registration(path,max_correspondence_distance_coarse,
                if target_id == source_id + 1:
                     # odometry
                     odometry = np.dot(transformation_icp, odometry)
-                    pose_graph.nodes.append(PoseGraphNode(np.linalg.inv(odometry)))
+                    pose_graph.nodes.append(
+                        PoseGraphNode(np.linalg.inv(odometry)))
                     pose_graph.edges.append(PoseGraphEdge(source_id, target_id,
-                                                          transformation_icp, information_icp, uncertain = False))
+                                                          transformation_icp, information_icp, uncertain=False))
                else:
                     # loop closure
                     pose_graph.edges.append(PoseGraphEdge(source_id, target_id,
-                                                          transformation_icp, information_icp, uncertain = True))
+                                                          transformation_icp, information_icp, uncertain=True))
 
      return pose_graph
 
+
 def load_images(path, ID):
-    
     """
-    Load a color and a depth image by path and image ID 
+    Load a color and a depth image by path and image ID
 
     """
     global camera_intrinsics
-    
+
     img_file = path + 'JPEGImages/%s.jpg' % (ID*LABEL_INTERVAL)
+    # print(ID*LABEL_INTERVAL)
     cad = cv2.imread(img_file)
 
     depth_file = path + 'depth/%s.png' % (ID*LABEL_INTERVAL)
-    reader = png.Reader(depth_file)
-    pngdata = reader.read()
-    depth = np.array(map(np.uint16, pngdata[2]))
+    # reader = png.Reader(depth_file)
+    # pngdata = reader.read()
+    # depth = np.array(map(np.uint16, pngdata[2]))
+    depth = np.array(Image.open(depth_file))
+    # print(depth.shape[1])
     pointcloud = convert_depth_frame_to_pointcloud(depth, camera_intrinsics)
-
 
     return (cad, pointcloud)
 
 
-def load_pcds(path, downsample = True, interval = 1):
+def load_pcds(path, downsample=True, interval=1):
+    """
+    load pointcloud by path and down samle (if True) based on voxel_size
 
     """
-    load pointcloud by path and down samle (if True) based on voxel_size 
 
-    """
-    
+    global voxel_size, camera_intrinsics
+    pcds = []
 
-    global voxel_size, camera_intrinsics 
-    pcds= []
-    
-    for Filename in xrange(len(glob.glob1(path+"JPEGImages","*.jpg"))/interval):
+    for Filename in xrange(len(glob.glob1(path+"JPEGImages", "*.jpg"))/interval):
         img_file = path + 'JPEGImages/%s.jpg' % (Filename*interval)
-        
+
         cad = cv2.imread(img_file)
         cad = cv2.cvtColor(cad, cv2.COLOR_BGR2RGB)
         depth_file = path + 'depth/%s.png' % (Filename*interval)
-        reader = png.Reader(depth_file)
-        pngdata = reader.read()
-        depth = np.array(map(np.uint16, pngdata[2]))
+        # reader = png.Reader(depth_file)
+        # pngdata = reader.read()
+        # depth = np.array(map(np.uint16, pngdata[2]))
+        depth = np.array(Image.open(depth_file))
         mask = depth.copy()
         depth = convert_depth_frame_to_pointcloud(depth, camera_intrinsics)
 
-
         source = PointCloud()
-        source.points = Vector3dVector(depth[mask>0])
-        source.colors = Vector3dVector(cad[mask>0])
+        source.points = Vector3dVector(depth[mask > 0])
+        source.colors = Vector3dVector(cad[mask > 0])
 
         if downsample == True:
-            pcd_down = voxel_down_sample(source, voxel_size = voxel_size)
-            estimate_normals(pcd_down, KDTreeSearchParamHybrid(radius = 0.002 * 2, max_nn = 30))
+            pcd_down = voxel_down_sample(source, voxel_size=voxel_size)
+            estimate_normals(pcd_down, KDTreeSearchParamHybrid(
+                radius=0.002 * 2, max_nn=30))
             pcds.append(pcd_down)
         else:
             pcds.append(source)
     return pcds
 
 
-def load_pcd(path, Filename, downsample = True, interval = 1):
+def load_pcd(path, Filename, downsample=True, interval=1):
+     """
+     load pointcloud by path and down samle (if True) based on voxel_size
 
      """
-     load pointcloud by path and down samle (if True) based on voxel_size 
-     
-     """
-    
 
-     global voxel_size, camera_intrinsics 
-    
- 
+     global voxel_size, camera_intrinsics
+
      img_file = path + 'JPEGImages/%s.jpg' % (Filename*interval)
 
      cad = cv2.imread(img_file)
      cad = cv2.cvtColor(cad, cv2.COLOR_BGR2RGB)
      depth_file = path + 'depth/%s.png' % (Filename*interval)
-     reader = png.Reader(depth_file)
-     pngdata = reader.read()
-     depth = np.array(map(np.uint16, pngdata[2]))
+    #  reader = png.Reader(depth_file)
+    #  pngdata = reader.read()
+    #  depth = np.array(map(np.uint16, pngdata[2]))
+     depth = np.array(Image.open(depth_file))
      mask = depth.copy()
      depth = convert_depth_frame_to_pointcloud(depth, camera_intrinsics)
 
@@ -361,7 +367,7 @@ if __name__ == "__main__":
 
         num_annotations = len(glob.glob1(path+"JPEGImages","*.jpg"))/LABEL_INTERVAL
 
-        for point_id in range(num_annotations):
+        for point_id in range(int(num_annotations)):
              Ts.append(pose_graph.nodes[point_id].pose)
         Ts = np.array(Ts)
         filename = path + 'transforms.npy'
--- utils/camera.py
+++ utils/camera.py
@@ -31,56 +31,60 @@ def nearest_neighbour(a, b):
 
 def convert_depth_frame_to_pointcloud(depth_image, camera_intrinsics):
         
-	"""
-	Convert the depthmap to a 3D point cloud
-	Parameters:
-	-----------
-	depth_frame : (m,n) uint16
-		The depth_frame containing the depth map
+    """
+    Convert the depthmap to a 3D point cloud
+    Parameters:
+    -----------
+    depth_frame : (m,n) uint16
+        The depth_frame containing the depth map
 
-	camera_intrinsics : dict 
+    camera_intrinsics : dict 
                 The intrinsic values of the depth imager in whose coordinate system the depth_frame is computed
-	Return:
-	----------
-	pointcloud : (m,n,3) float
-		The corresponding pointcloud in meters
+    Return:
+    ----------
+    pointcloud : (m,n,3) float
+        The corresponding pointcloud in meters
 
-	"""
-	
-	[height, width] = depth_image.shape
+    """
+    
+    # [height, width] = depth_image.shape
+    # print(depth_image.shape[0])
+    height = depth_image.shape[0]
+    width = depth_image.shape[1]
 
-	nx = np.linspace(0, width-1, width)
-	ny = np.linspace(0, height-1, height)
-	u, v = np.meshgrid(nx, ny)
-	x = (u.flatten() - float(camera_intrinsics['ppx']))/float(camera_intrinsics['fx'])
-	y = (v.flatten() - float(camera_intrinsics['ppy']))/float(camera_intrinsics['fy'])
-        depth_image = depth_image*1.0/65535*8.0
-	z = depth_image.flatten()
-	x = np.multiply(x,z)
-	y = np.multiply(y,z)
 
-        pointcloud = np.dstack((x,y,z)).reshape((depth_image.shape[0],depth_image.shape[1],3))
+    nx = np.linspace(0, width-1, width)
+    ny = np.linspace(0, height-1, height)
+    u, v = np.meshgrid(nx, ny)
+    x = (u.flatten() - float(camera_intrinsics['ppx']))/float(camera_intrinsics['fx'])
+    y = (v.flatten() - float(camera_intrinsics['ppy']))/float(camera_intrinsics['fy'])
+    depth_image = depth_image*1.0/65535*8.0
+    z = depth_image.flatten()
+    x = np.multiply(x,z)
+    y = np.multiply(y,z)
 
-        return pointcloud
+    pointcloud = np.dstack((x,y,z)).reshape((depth_image.shape[0],depth_image.shape[1],3))
+
+    return pointcloud
 
 def project_point_to_pixel(point, serialnumber, camera_intrinsics):
-        depth_image
+    depth_image
 
-	height = 480
-        width = 640
+    height = 480
+    width = 640
 
-	nx = np.linspace(0, width-1, width)
-	ny = np.linspace(0, height-1, height)
-	u, v = np.meshgrid(nx, ny)
-	x = (u.flatten() - camera_intrinsics[serialnumber]['ppx'])/camera_intrinsics[serialnumber]['fx']
-	y = (v.flatten() - camera_intrinsics[serialnumber]['ppy'])/camera_intrinsics[serialnumber]['fy']
-        z = np.ones(height*width)*point[2]
-	x = np.multiply(x,z)
-	y = np.multiply(y,z)
+    nx = np.linspace(0, width-1, width)
+    ny = np.linspace(0, height-1, height)
+    u, v = np.meshgrid(nx, ny)
+    x = (u.flatten() - camera_intrinsics[serialnumber]['ppx'])/camera_intrinsics[serialnumber]['fx']
+    y = (v.flatten() - camera_intrinsics[serialnumber]['ppy'])/camera_intrinsics[serialnumber]['fy']
+    z = np.ones(height*width)*point[2]
+    x = np.multiply(x,z)
+    y = np.multiply(y,z)
 
-        pointcloud = np.dstack((x,y,z)).reshape((depth_image.shape[0],depth_image.shape[1],3))
-        dis, index = nearest_neighbour(point, pointcloud)
+    pointcloud = np.dstack((x,y,z)).reshape((depth_image.shape[0],depth_image.shape[1],3))
+    dis, index = nearest_neighbour(point, pointcloud)
         
 
-        return index
+    return index
 
