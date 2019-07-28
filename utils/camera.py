39a9d03f086dc8ab84593573542d8e67521c3c55
Ubuntu <ubuntu@ip-172-31-17-102.us-east-2.compute.internal>
2019-07-28 20:21:10 +0000

    Fixed the tab/space inconsistency

--- utils/camera.py
+++ utils/camera.py
@@ -8,6 +8,7 @@ Functions related to data acquisition with cameras.
 
 import numpy as np
 
+
 def nearest_neighbour(a, b):
     """
     find the nearest neighbours of a in b using KDTree
@@ -28,59 +29,62 @@ def nearest_neighbour(a, b):
     return (dist, index)
 
 
-
 def convert_depth_frame_to_pointcloud(depth_image, camera_intrinsics):
-        
-	"""
-	Convert the depthmap to a 3D point cloud
-	Parameters:
-	-----------
-	depth_frame : (m,n) uint16
-		The depth_frame containing the depth map
+    """
+    Convert the depthmap to a 3D point cloud
+    Parameters:
+    -----------
+    depth_frame : (m,n) uint16
+            The depth_frame containing the depth map
 
-	camera_intrinsics : dict 
-                The intrinsic values of the depth imager in whose coordinate system the depth_frame is computed
-	Return:
-	----------
-	pointcloud : (m,n,3) float
-		The corresponding pointcloud in meters
+    camera_intrinsics : dict 
+            The intrinsic values of the depth imager in whose coordinate system the depth_frame is computed
+    Return:
+    ----------
+    pointcloud : (m,n,3) float
+            The corresponding pointcloud in meters
 
-	"""
-	
-	[height, width] = depth_image.shape
+    """
 
-	nx = np.linspace(0, width-1, width)
-	ny = np.linspace(0, height-1, height)
-	u, v = np.meshgrid(nx, ny)
-	x = (u.flatten() - float(camera_intrinsics['ppx']))/float(camera_intrinsics['fx'])
-	y = (v.flatten() - float(camera_intrinsics['ppy']))/float(camera_intrinsics['fy'])
-        depth_image = depth_image*1.0/65535*8.0
-	z = depth_image.flatten()
-	x = np.multiply(x,z)
-	y = np.multiply(y,z)
+    [height, width] = depth_image.shape
 
-        pointcloud = np.dstack((x,y,z)).reshape((depth_image.shape[0],depth_image.shape[1],3))
+    nx = np.linspace(0, width-1, width)
+    ny = np.linspace(0, height-1, height)
+    u, v = np.meshgrid(nx, ny)
+    x = (u.flatten() -
+         float(camera_intrinsics['ppx']))/float(camera_intrinsics['fx'])
+    y = (v.flatten() -
+         float(camera_intrinsics['ppy']))/float(camera_intrinsics['fy'])
+    depth_image = depth_image*1.0/65535*8
+    z = depth_image.flatten()
+    x = np.multiply(x, z)
+    y = np.multiply(y, z)
 
-        return pointcloud
+    pointcloud = np.dstack((x, y, z)).reshape(
+        (depth_image.shape[0], depth_image.shape[1], 3))
+
+    return pointcloud
+
 
 def project_point_to_pixel(point, serialnumber, camera_intrinsics):
-        depth_image
+    depth_image
 
-	height = 480
-        width = 640
+    height = 480
+    width = 640
 
-	nx = np.linspace(0, width-1, width)
-	ny = np.linspace(0, height-1, height)
-	u, v = np.meshgrid(nx, ny)
-	x = (u.flatten() - camera_intrinsics[serialnumber]['ppx'])/camera_intrinsics[serialnumber]['fx']
-	y = (v.flatten() - camera_intrinsics[serialnumber]['ppy'])/camera_intrinsics[serialnumber]['fy']
-        z = np.ones(height*width)*point[2]
-	x = np.multiply(x,z)
-	y = np.multiply(y,z)
+    nx = np.linspace(0, width-1, width)
+    ny = np.linspace(0, height-1, height)
+    u, v = np.meshgrid(nx, ny)
+    x = (u.flatten() - camera_intrinsics[serialnumber]
+         ['ppx'])/camera_intrinsics[serialnumber]['fx']
+    y = (v.flatten() - camera_intrinsics[serialnumber]
+         ['ppy'])/camera_intrinsics[serialnumber]['fy']
+    z = np.ones(height*width)*point[2]
+    x = np.multiply(x, z)
+    y = np.multiply(y, z)
 
-        pointcloud = np.dstack((x,y,z)).reshape((depth_image.shape[0],depth_image.shape[1],3))
-        dis, index = nearest_neighbour(point, pointcloud)
-        
+    pointcloud = np.dstack((x, y, z)).reshape(
+        (depth_image.shape[0], depth_image.shape[1], 3))
+    dis, index = nearest_neighbour(point, pointcloud)
 
-        return index
-
+    return index
